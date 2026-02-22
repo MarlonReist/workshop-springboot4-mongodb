b9ffc77cadc7f726fe02f8b28aff1dfbad07855a
Marlon Reis <marlonreisdev@gmail.com>
2026-02-22 16:59:59 -0300

    User insertion using POST

--- src/main/java/com/marlondev/workshopmongodb/services/UserService.java
+++ src/main/java/com/marlondev/workshopmongodb/services/UserService.java
@@ -3,6 +3,7 @@ package com.marlondev.workshopmongodb.services;
 import java.util.List;
 
 import com.marlondev.workshopmongodb.domain.User;
+import com.marlondev.workshopmongodb.dto.UserDTO;
 import com.marlondev.workshopmongodb.repository.UserRepository;
 import com.marlondev.workshopmongodb.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,6 +23,14 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 
 }
