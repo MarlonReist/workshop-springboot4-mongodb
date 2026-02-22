5003daf8f492217c388ae2ad509b79aa61864fb5
Marlon Reis <marlonreisdev@gmail.com>
2026-02-22 16:51:41 -0300

    Retrieving a user by ID

--- src/main/java/com/marlondev/workshopmongodb/services/UserService.java
+++ src/main/java/com/marlondev/workshopmongodb/services/UserService.java
@@ -4,8 +4,10 @@ import java.util.List;
 
 import com.marlondev.workshopmongodb.domain.User;
 import com.marlondev.workshopmongodb.repository.UserRepository;
+import com.marlondev.workshopmongodb.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -16,4 +18,10 @@ public class UserService {
     public List<User> findAll() {
         return repo.findAll();
     }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
 }
