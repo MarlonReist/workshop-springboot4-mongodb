fbeb3bd81752aa68ab10f9f0c6e840d4103bb270
Marlon Reis <marlonreisdev@gmail.com>
2026-02-23 16:12:26 -0300

    Simple consult with @Query

--- src/main/java/com/marlondev/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/marlondev/workshopmongodb/repository/PostRepository.java
@@ -3,6 +3,7 @@ package com.marlondev.workshopmongodb.repository;
 import com.marlondev.workshopmongodb.domain.Post;
 import com.marlondev.workshopmongodb.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -10,6 +11,9 @@ import java.util.List;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> searchTitle(String text);
 
     List<Post> findByTitleContainingIgnoreCase(String text);
 
