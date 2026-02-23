e248e61c9c2de6c2112c89b0891683e6f974e36f
Marlon Reis <marlonreisdev@gmail.com>
2026-02-23 15:36:21 -0300

    retrieving post by id

---
+++ src/main/java/com/marlondev/workshopmongodb/services/PostService.java
@@ -0,0 +1,25 @@
+package com.marlondev.workshopmongodb.services;
+
+import com.marlondev.workshopmongodb.domain.Post;
+import com.marlondev.workshopmongodb.domain.User;
+import com.marlondev.workshopmongodb.dto.UserDTO;
+import com.marlondev.workshopmongodb.repository.PostRepository;
+import com.marlondev.workshopmongodb.repository.UserRepository;
+import com.marlondev.workshopmongodb.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+}
