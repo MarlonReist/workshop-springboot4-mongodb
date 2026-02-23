497f490a79f19221273c95249583314a38b1d00c
Marlon Reis <marlonreisdev@gmail.com>
2026-02-23 14:44:56 -0300

    Endpoint to return a user's posts

--- src/main/java/com/marlondev/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/marlondev/workshopmongodb/resources/UserResource.java
@@ -4,6 +4,7 @@ import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
+import com.marlondev.workshopmongodb.domain.Post;
 import com.marlondev.workshopmongodb.domain.User;
 import com.marlondev.workshopmongodb.dto.UserDTO;
 import com.marlondev.workshopmongodb.services.UserService;
@@ -52,5 +53,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/{id}/posts", method = RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
