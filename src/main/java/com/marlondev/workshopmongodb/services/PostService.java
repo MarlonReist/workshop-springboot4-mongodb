a85dde96632d0ae211e7825e132c104c8b7b9f7f
Marlon Reis <marlonreisdev@gmail.com>
2026-02-23 16:35:20 -0300

    consultation with various criteria

--- src/main/java/com/marlondev/workshopmongodb/services/PostService.java
+++ src/main/java/com/marlondev/workshopmongodb/services/PostService.java
@@ -9,6 +9,7 @@ import com.marlondev.workshopmongodb.services.exceptions.ObjectNotFoundException
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -25,5 +26,9 @@ public class PostService {
 
     public List<Post> findByTitle(String text){
         return repo.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text,Date minDate, Date maxDate) {
+        return repo.fullSearch(text,minDate,maxDate);
     }
 }
